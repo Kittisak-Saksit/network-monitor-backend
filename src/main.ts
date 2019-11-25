dcbe7bb559de0e9640ec4479f4e4c31fe0ba01d8
Kittisak-Saksit <kittisaksaksit@hotmail.com>
2019-11-25 17:43:27 +0700

    change port server

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.use(middleware())
-  await app.listen(process.env.PORT)
+  await app.listen(process.env.PORT || 7000)
 }
 bootstrap()
